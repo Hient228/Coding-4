2761e2df7ad22fb2fbf131b71573d063562daf58
Hient <hient@usf.edu>
2025-02-01 20:53:51 -0500

    Task 1 - Customer Discounts

--- cc_4.js
+++ cc_4.js
@@ -1,1 +1,10 @@
+//Task 1: If Statements
+let purchaseAmount = 170; 
+let finalAmount = purchaseAmount;
 
+if (purchaseAmount > 100) {
+    finalAmount = purchaseAmount * 0.1;
+    console.log("Discount applied")
+}
+
+console.log(`Final amount after discount: $${finalAmount}`);
